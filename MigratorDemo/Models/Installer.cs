4926bf5000158ee02742b00c3ab038b742cb6103
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-30 11:08:41 +0100

    Add project files.

---
+++ MigratorDemo/Models/Installer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratorDemo.Models
+{
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing our new program");
+        }
+    }
+}
---
+++ MigratorDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using MigratorDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbMigrator dbMigrator = new DbMigrator(new Logger());
+
+            dbMigrator.Migrate();
+        }
+    }
+}
